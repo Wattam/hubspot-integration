791cbaebdc8acab101568827eb7718f53edb6993
Wattam <nalbertwattam@gmail.com>
2025-04-22 13:24:13 -0300

    feat: create base project

---
+++ src/main/java/com/meetime/hubspotintegration/HubspotIntegrationApplication.java
@@ -0,0 +1,12 @@
+package com.meetime.hubspotintegration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HubspotIntegrationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HubspotIntegrationApplication.class, args);
+    }
+}
